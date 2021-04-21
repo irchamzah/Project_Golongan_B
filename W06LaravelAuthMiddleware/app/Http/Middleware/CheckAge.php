a35ccce3958ba1ae046060d33067b9d9acb96ac5
irchamzah <71950891+irchamzah@users.noreply.github.com>
2021-04-21 15:13:38 +0700

    Minggu 6/3

---
+++ W06LaravelAuthMiddleware/app/Http/Middleware/CheckAge.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->age <= 200) {
+            return redirect('home');
+        }
+        
+        return $next($request);
+    }
+}
+
+class BeforeMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
+
+class AfterMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        return $response;
+    }
+}
